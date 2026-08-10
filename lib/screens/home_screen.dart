f36d78fa67bf7c25b7110550dc3df2de53c735b7
TheFlutterSensei <hi@fluttersensei.com>
2026-08-10 17:03:14 +0530

    Completed Positioned Widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,15 +11,31 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(width: 300, height: 200, color: Colors.teal),
-          const Text(
-            'UI Engineering',
-            style: TextStyle(fontSize: 24, color: Colors.white),
-          ),
-          Icon(Icons.star),
-        ],
+      body: Center(
+        child: Stack(
+          children: [
+            Container(
+              width: 300,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            const Text(
+              'UI Engineering',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.close, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
