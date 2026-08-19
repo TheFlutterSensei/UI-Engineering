a64fc4cc889b36307fdfff2f86fe3a370b0b25d2
TheFlutterSensei <hi@fluttersensei.com>
2026-08-19 11:04:24 +0530

    Completed 01.20

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ui_engineering/widgets/profile_header.dart';
 import 'package:ui_engineering/widgets/stat_card.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,21 +12,45 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16),
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
-        children: [
-          StatCard(icon: Icons.people, title: 'Users', value: '12,450'),
-          StatCard(icon: Icons.shopping_cart, title: 'Orders', value: '856'),
-          StatCard(
-            icon: Icons.attach_money,
-            title: 'Revenue',
-            value: '\$24,500',
-          ),
-          StatCard(icon: Icons.inventory_2, title: 'products', value: '1,240'),
-        ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          print(constraints.maxWidth);
+
+          int columns = 2;
+
+          if (constraints.maxWidth > 900) {
+            columns = 4;
+          } else if (constraints.maxWidth > 600) {
+            columns = 3;
+          } else {
+            columns = 2;
+          }
+
+          return GridView.count(
+            crossAxisCount: columns,
+            padding: const EdgeInsets.all(16),
+            crossAxisSpacing: 16,
+            mainAxisSpacing: 16,
+            children: [
+              StatCard(icon: Icons.people, title: 'Users', value: '12,450'),
+              StatCard(
+                icon: Icons.shopping_cart,
+                title: 'Orders',
+                value: '856',
+              ),
+              StatCard(
+                icon: Icons.attach_money,
+                title: 'Revenue',
+                value: '\$24,500',
+              ),
+              StatCard(
+                icon: Icons.inventory_2,
+                title: 'products',
+                value: '1,240',
+              ),
+            ],
+          );
+        },
       ),
     );
   }
