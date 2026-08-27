401b7db9c1c9fd6744f7f42978265ba118d93f92
TheFlutterSensei <hi@fluttersensei.com>
2026-08-27 11:03:37 +0530

    Completed 02.02

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ui_engineering/constants/spacing.dart';
-import 'package:ui_engineering/constants/text_styles.dart';
-import 'package:ui_engineering/widgets/bento_card.dart';
-import 'package:ui_engineering/widgets/profile_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,23 +9,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form(
+        key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(Spacing.lg),
           child: Column(
             children: [
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: Spacing.md),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: Spacing.md),
-              ElevatedButton(onPressed: () {}, child: const Text('Submit')),
+              ElevatedButton(
+                onPressed: () {
+                  _formKey.currentState!.validate();
+                },
+                child: const Text('Submit'),
+              ),
             ],
           ),
         ),
