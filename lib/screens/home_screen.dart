a88607fb31a968a5653487f9afe6d911b21ddd2f
TheFlutterSensei <hi@fluttersensei.com>
2026-08-31 10:41:21 +0530

    Completed 02.13

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,47 +22,53 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(Spacing.lg),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.text,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(labelText: 'Name'),
-                validator: (value) {
-                  return Validators.checkRequired(value, 'Name');
-                },
-              ),
-              SizedBox(height: Spacing.md),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: Validators.checkEmail,
-              ),
-              SizedBox(height: Spacing.md),
-              TextFormField(
-                keyboardType: TextInputType.phone,
-                textInputAction: TextInputAction.next,
-                decoration: const InputDecoration(labelText: 'Phone'),
-                validator: (value) {
-                  return Validators.checkRequired(value, 'Phone Number');
-                },
-              ),
-              SizedBox(height: Spacing.md),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                textInputAction: TextInputAction.done,
-                decoration: const InputDecoration(labelText: 'Age'),
-                validator: (value) {
-                  return Validators.checkRequired(value, 'Age');
-                },
-              ),
-            ],
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(Spacing.lg),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(labelText: 'Name'),
+                  validator: (value) {
+                    return Validators.checkRequired(value, 'Name');
+                  },
+                ),
+                SizedBox(height: Spacing.md),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(labelText: 'Email'),
+                  validator: Validators.checkEmail,
+                ),
+                SizedBox(height: Spacing.md),
+                TextFormField(
+                  keyboardType: TextInputType.phone,
+                  textInputAction: TextInputAction.next,
+                  decoration: const InputDecoration(labelText: 'Phone'),
+                  validator: (value) {
+                    return Validators.checkRequired(value, 'Phone Number');
+                  },
+                ),
+                SizedBox(height: Spacing.md),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  textInputAction: TextInputAction.done,
+                  decoration: const InputDecoration(labelText: 'Age'),
+                  validator: (value) {
+                    return Validators.checkRequired(value, 'Age');
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
