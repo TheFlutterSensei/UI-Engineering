4edead396a09c1663dc9e2c3a28629c4dadc228b
TheFlutterSensei <hi@fluttersensei.com>
2026-09-01 11:09:03 +0530

    Completed 02.17

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_engineering/constants/spacing.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,34 +9,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool _notificationsEnabled = false;
-  bool _darkModeEnabled = false;
+  String? _selectedCountry;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        child: Column(
-          children: [
-            SwitchListTile(
-              title: const Text('Notifications'),
-              value: _notificationsEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _notificationsEnabled = value;
-                });
-              },
-            ),
-            SwitchListTile(
-              title: const Text('Dark Mode'),
-              value: _darkModeEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _darkModeEnabled = value;
-                });
-              },
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(Spacing.lg),
+        child: Form(
+          child: Column(
+            children: [
+              DropdownButtonFormField(
+                initialValue: _selectedCountry,
+                decoration: const InputDecoration(labelText: 'Country'),
+                items: const [
+                  DropdownMenuItem(value: 'india', child: Text('India')),
+                  DropdownMenuItem(value: 'usa', child: Text('USA')),
+                  DropdownMenuItem(value: 'uk', child: Text('UK')),
+                  DropdownMenuItem(value: 'canada', child: Text('Canad')),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    _selectedCountry = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
