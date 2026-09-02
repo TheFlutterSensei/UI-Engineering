20287e2219a391254592721ae3536ee82371c738
TheFlutterSensei <hi@fluttersensei.com>
2026-09-02 12:56:55 +0530

    Completed 02.22

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,11 +9,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String formatDate(DateTime date) {
-    return '${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}';
-  }
-
   DateTime? _selectedDate;
+  TimeOfDay? _selectedTime;
 
   @override
   Widget build(BuildContext context) {
@@ -24,27 +21,59 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ElevatedButton(
-                onPressed: () async {
-                  final selectedDate = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime.now(),
-                    lastDate: DateTime(2030),
-                  );
+              Wrap(
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      final selectedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime.now(),
+                        lastDate: DateTime(2030),
+                      );
 
-                  if (selectedDate != null) {
-                    setState(() {
-                      _selectedDate = selectedDate;
-                    });
-                  }
-                },
-                child: const Text('Select Date'),
+                      if (selectedDate != null) {
+                        setState(() {
+                          _selectedDate = selectedDate;
+                        });
+                      }
+                    },
+                    child: const Text('Select Date'),
+                  ),
+                  SizedBox(width: Spacing.md),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final selectTime = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                        builder: (context, child) {
+                          return MediaQuery(
+                            data: MediaQuery.of(
+                              context,
+                            ).copyWith(alwaysUse24HourFormat: false),
+                            child: child!,
+                          );
+                        },
+                      );
+
+                      if (selectTime != null) {
+                        setState(() {
+                          _selectedTime = selectTime;
+                        });
+                      }
+                    },
+                    child: const Text('Select Time'),
+                  ),
+                ],
               ),
 
               SizedBox(height: Spacing.lg),
 
-              if (_selectedDate != null) Text(formatDate(_selectedDate!)),
+              if (_selectedDate != null && _selectedTime != null)
+                Text(
+                  'Scheduled for ${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}'
+                  'at ${_selectedTime!.hourOfPeriod}:${_selectedTime!.minute} ${_selectedTime!.period == DayPeriod.am ? 'AM' : 'PM'}',
+                ),
             ],
           ),
         ),
