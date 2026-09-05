e0ede7cf2294d125d085d8898ba6b343dd4c1f0b
TheFlutterSensei <hi@fluttersensei.com>
2026-09-05 11:47:40 +0530

    Completed 02.24

---
+++ lib/constants/form_field_design.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FormFieldDesign {
+  static const InputDecoration textFieldDesign = InputDecoration(
+    filled: true,
+    fillColor: Color.fromARGB(10, 0, 0, 0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16)),
+      borderSide: BorderSide.none,
+    ),
+  );
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_engineering/constants/form_field_design.dart';
 import 'package:ui_engineering/constants/spacing.dart';
 import 'package:ui_engineering/validators/validators.dart';
 
@@ -15,30 +16,57 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up to Get Started'),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(Spacing.lg),
         child: Form(
           key: _formKey,
           autovalidateMode: AutovalidateMode.onUnfocus,
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(labelText: 'Name'),
-                validator: (value) => Validators.checkRequired(value, 'Name'),
+                keyboardType: TextInputType.name,
+                decoration: FormFieldDesign.textFieldDesign.copyWith(
+                  labelText: 'Your Name',
+                ),
+                validator: (value) =>
+                    Validators.checkRequired(value, 'Your name'),
               ),
-              SizedBox(height: Spacing.md),
+              SizedBox(height: Spacing.sm),
               TextFormField(
-                decoration: const InputDecoration(labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+                decoration: FormFieldDesign.textFieldDesign.copyWith(
+                  labelText: 'Your Email ID',
+                ),
                 validator: (value) => Validators.checkEmail(value),
               ),
-              SizedBox(height: Spacing.md),
-              ElevatedButton(
-                onPressed: () {
-                  _formKey.currentState!.validate();
-                },
-                child: const Text('Check Field'),
+              SizedBox(height: Spacing.sm),
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: FormFieldDesign.textFieldDesign.copyWith(
+                  labelText: 'Your Phone Number',
+                ),
+                validator: (value) =>
+                    Validators.checkRequired(value, 'your Phone Number'),
+              ),
+              SizedBox(height: Spacing.sm),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                  ),
+                  onPressed: () {
+                    _formKey.currentState!.validate();
+                  },
+                  child: Text('Sign up'),
+                ),
               ),
             ],
           ),
