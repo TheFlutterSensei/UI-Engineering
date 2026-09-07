a82c49add4e81e39dc9ae7d7e00f4fce06d2adb6
TheFlutterSensei <hi@fluttersensei.com>
2026-09-07 13:41:05 +0530

    Completed 03.02

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ui_engineering/models/user.dart';
+import 'package:ui_engineering/constants/spacing.dart';
+import 'package:ui_engineering/models/product.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,11 +10,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final user = User(
-    name: 'Flutter Sensei',
-    email: 'contact@fluttersensei.com',
-    age: 30,
-  );
+  final products = [
+    Product(
+      name: 'Wireless Headphones',
+      description: 'Comfortable wireless headphones',
+      price: 2499.00,
+    ),
+    Product(
+      name: 'Mechanical Keyboard',
+      description: 'A Compact Mechanical Keyboard',
+      price: 3499.00,
+    ),
+    Product(
+      name: 'Wireless Mouse',
+      description: 'A lightweight wireless mouse',
+      price: 1499.00,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +35,11 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(user.name),
-            Text(user.email),
-            Text('Age: ${user.age}'),
+            Text('${products.length} products'),
+            SizedBox(height: Spacing.lg),
+            Text(
+              '\$${products[1].name}\n${products[1].description}\n\$${products[1].price}',
+            ),
           ],
         ),
       ),
