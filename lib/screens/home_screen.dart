d2b9262330cf0ca459f7146e56a6d78a6ef861ab
TheFlutterSensei <hi@fluttersensei.com>
2026-09-07 21:00:21 +0530

    Completed 03.11

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ui_engineering/constants/form_field_design.dart';
 import 'package:ui_engineering/constants/spacing.dart';
 import 'package:ui_engineering/data/products.dart';
+import 'package:ui_engineering/models/product.dart';
 import 'package:ui_engineering/widgets/product_card.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,6 +15,16 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   String _searchQuery = '';
 
+  List<Product> get _filteredproducts {
+    if (_searchQuery.isEmpty) {
+      return products;
+    }
+
+    return products.where((product) {
+      return product.name.contains(_searchQuery);
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.all(Spacing.md),
             child: TextField(
               decoration: FormFieldDesign.textFieldDesign.copyWith(
-                hintText: 'Search',
+                hintText: 'Search products',
                 prefixIcon: Icon(Icons.search, size: 16),
               ),
               onChanged: (value) {
@@ -37,7 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Expanded(
             child: GridView.builder(
               padding: EdgeInsets.all(Spacing.md),
-              itemCount: products.length,
+              itemCount: _filteredproducts.length,
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                 maxCrossAxisExtent: 250,
                 childAspectRatio: 0.8,
@@ -45,7 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 crossAxisSpacing: Spacing.md,
               ),
               itemBuilder: (context, index) {
-                final product = products[index];
+                final product = _filteredproducts[index];
 
                 return ProductCard(product: product);
               },
