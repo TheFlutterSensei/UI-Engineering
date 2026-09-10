a330a0afcf8209b2df747b8fc77d8c01a96a6eb2
TheFlutterSensei <hi@fluttersensei.com>
2026-09-10 12:15:11 +0530

    Completed 04.07

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ui_engineering/constants/spacing.dart';
+import 'package:ui_engineering/constants/text_styles.dart';
 import 'package:ui_engineering/models/product.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -10,7 +12,34 @@ class ProductScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Products')),
-      body: Center(child: Text(product.name)),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(Spacing.md),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Image.network(
+                  product.image,
+                  width: double.infinity,
+                  height: 250,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: Spacing.md),
+              Text(product.name, style: TextStyles.title),
+              const SizedBox(height: Spacing.sm),
+              Text(product.description),
+              const SizedBox(height: Spacing.md),
+              Text(
+                product.formattedPrice,
+                style: TextStyles.heading.copyWith(color: Colors.green),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
