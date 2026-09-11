b22dba0ae58f1aa058f156edeb56206843ec96d5
TheFlutterSensei <hi@fluttersensei.com>
2026-09-11 23:23:22 +0530

    Completed 04.22

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -11,6 +11,38 @@ class EditProfileScreen extends StatefulWidget {
 class _EditProfileScreenState extends State<EditProfileScreen> {
   final _nameController = TextEditingController();
 
+  Future<void> _showDiscardDialog() async {
+    final shouldDiscard = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Discard Changed'),
+          content: const Text(
+            'Your changes will be lost if you leave this screen',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: const Text('Stay'),
+            ),
+            FilledButton(
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: const Text('Discard'),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (shouldDiscard == true && mounted) {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   void dispose() {
     _nameController.dispose();
@@ -21,6 +53,13 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   Widget build(BuildContext context) {
     return PopScope(
       canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) {
+          return;
+        }
+
+        _showDiscardDialog();
+      },
       child: Scaffold(
         appBar: AppBar(title: const Text('Edit Profile')),
         body: Padding(
